19ad6f90c3ac8622ce5a5124ec2049268e5be4bf
Eric Stone <Eric.Stone@carvana.com>
2021-07-27 16:10:05 -0700

    Modified welcome screen

---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import HeaderTextBlock from "@carvana/showroom-content/TextBlock/Header";
+import Input from '@carvana/showroom-forms/Input';
+import * as sc from "./welcome.styles";
+
+const WelcomeScreen = () => {
+  return (
+    <sc.ContentWrapper>
+      <HeaderTextBlock size="large">
+        <HeaderTextBlock.Title>Welcome to the Carvana Career Profile</HeaderTextBlock.Title>
+        <HeaderTextBlock.Description>
+          Please sign up to continue to your career portal!
+        </HeaderTextBlock.Description>
+      </HeaderTextBlock>
+      <sc.EmailTextContainer>
+        <Input label="Email" width={400} mode="dark"/>
+      </sc.EmailTextContainer>
+      <sc.ButtonContainer>
+        <sc.StyledButton>
+          Sign up
+        </sc.StyledButton>
+      </sc.ButtonContainer>
+      <sc.AccountExistsTextContainer>
+        Already have a Career Profile? 
+        <sc.LinkText href='/login'>&#160;Log In</sc.LinkText>
+      </sc.AccountExistsTextContainer>
+
+    </sc.ContentWrapper>
+  );
+};
+
+export default WelcomeScreen;
