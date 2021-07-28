5c9388a8a2754630905c70cec2f362abb46cba5f
lelanddaniel <88048565+lelanddaniel@users.noreply.github.com>
2021-07-28 11:10:19 -0700

    More header work (#10)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 import * as sc from './header.styles';
 import Primary from '../../assets/images/PrimaryLogo.svg';
 
 const Header = () => {
-
   return (
     <sc.HeaderWrapper>
-      <sc.ImageWrapper src={Primary} alt='Primary'/>
-      <sc.FastLane>Fast Lane</sc.FastLane>
-      <sc.PipeDivider />
-      <sc.CareerProfile>Career Profile</sc.CareerProfile>
-
+      <sc.ContentWrapper>
+        <sc.ImageWrapper src={Primary} alt='Primary'/>
+        <sc.FastLane>Fast Lane</sc.FastLane>
+        <sc.PipeDivider />
+        <sc.CareerProfile>Career Profile</sc.CareerProfile>
+      </sc.ContentWrapper>
     </sc.HeaderWrapper>
   )
 }
