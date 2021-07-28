1f9326c925adf5cb1fa0df4a25540bc072772179
Leland Daniel <leland.daniel@carvana.com>
2021-07-28 09:50:51 -0700

    Added global styles.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import Header from './components/header'
 import Routes from './routes';
-import styled from "styled-components";
 import { ThemeProvider } from "@carvana/showroom-theme";
-
-const MainWrapper = styled.div`
-  height: 100vh;
-  width: 100%;
-  font-family: ${(props) => props.theme.font.family};
-  background: #CFEAF2; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to top,
-    #eef2f3,
-    #CFEAF2
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to top,
-    #eef2f3,
-    #CFEAF2
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-`;
 
 function App() {
   return (
     <ThemeProvider>
-      <MainWrapper>
-        <Header />
-        <Routes />
-
-      </MainWrapper>
+      <Header />
+      <Routes />
     </ThemeProvider>
   );
 }
