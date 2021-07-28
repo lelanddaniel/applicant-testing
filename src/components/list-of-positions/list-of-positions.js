9ef164db6e4f59bcf22223d2ac79c68acb5d22b4
Leland Daniel <leland.daniel@carvana.com>
2021-07-28 16:24:25 -0700

    Added Yup and more work for positions.

--- src/components/list-of-positions/list-of-positions.js
+++ src/components/list-of-positions/list-of-positions.js
@@ -6,10 +6,21 @@ const ListOfPositions = () => {
 
   return (
     <sc.Wrapper>
-      <SectionHeader>Featured positions</SectionHeader>
-      <SubSectionHeader>Be part of the team disrupting the car-buying status quo.</SubSectionHeader>
+      <sc.HelpGetToKnow>
+        <div>
+          <SectionHeader>Help us get to know you</SectionHeader>
+          <SubSectionHeader>Your profile helps our recruiting team know who you are and what fits you.</SubSectionHeader>
+        </div>
+        <div>
+          <sc.HelpGetToKnowButton>Complete profile</sc.HelpGetToKnowButton>
+        </div>
+      </sc.HelpGetToKnow>
       <sc.PositionsSection>
-        
+        <SectionHeader>Featured positions</SectionHeader>
+        <SubSectionHeader>Be part of the team disrupting the car-buying status quo.</SubSectionHeader>
+        <sc.PositionsListSection>
+
+        </sc.PositionsListSection>
       </sc.PositionsSection>
 
       <SectionHeader>Related blog post</SectionHeader>
