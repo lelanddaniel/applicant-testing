003e9a8654fd67c1f8a9990448c501f4e7d09812
lelanddaniel <88048565+lelanddaniel@users.noreply.github.com>
2021-07-28 17:30:04 -0700

    Created a navigation config, to make it easier to add more later. (#17)

---
+++ src/assets/components/navigation/navigation.config.js
@@ -0,0 +1,13 @@
+const NavigationConfig = [
+  {
+    id: 1,
+    title: 'Home',
+    path: '/positions'
+  },{
+    id: 2,
+    title: 'Applications',
+    path: '/applications'
+  }
+]
+
+export default NavigationConfig;
